3ff26baa0827a3cb0a2cb24148113c0af5f80287
daogen <daoyeyemi@aol.com>
2020-08-05 16:50:06 -0700

    created prototype time block

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create timeblocks
+$("#plannerSpace").append()
+$("#timeBox").append("<span class='input-group-text' id='basic-addon1'>9 am</span>")
+$("#inputBox").append("<input type='text' class='form-control'>")
+$("#saveBox").append("<button class='btn btn-outline-secondary' type='button' id='button-addon2'>Save</button>")
+// Create buttons for saving info in box  
+// Create date on top of page  
+// Color code according to hour in workday
