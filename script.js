41906f99e10e9d33dbf13411d5f049b8822716bf
daogen <daoyeyemi@aol.com>
2020-08-06 00:48:17 -0700

    had to start over and define some variables

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
+$(document).ready(function(){
+    // test
+    const trial = false;
+    // get times from moment.js
+    const present = moment().format('MMMM Do YYYY');
+    
+    let nowHour24 = moment().format('H');
+    let nowHour12 = moment().format('h');
 
-    // var plannerSpace = $("");
-    // for (time = 9; time < 17; time++) {
-    //     value = time - 9;
-// Create timeblocks
-$("#plannerSpace").append("<div class='row'>");
-// console.log(row);
-$(".row").append("<div class='col-md-2' id='timeBox'>");
-$("#timeBox").append('<span class="input-group-text" id="basic-addon1">@</span>');
-$(".row").append("<div class='col-md-8' id='inputBox'>");
-$("#inputBox").append('<input type="text" class="form-control" placeholder="Enter tasks here..." aria-label="Username" aria-describedby="basic-addon1">');
-$(".row").append("<div class='col-md-2' id='saveBox'>");
-$("#saveBox").append('<button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>');
+    if (test) {
+        nowHour24 = 13;
+        nowHour12 = 1;
+    }
 
+    let $dayHeading = $('')
 
-// Time: 9 am
-
-// Create buttons for saving info in box  
-// Create date on top of page  
-// Color code according to hour in work day
+})
