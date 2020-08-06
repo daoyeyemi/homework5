b53ea3095102592709b5286bafc05776aded1583
daogen <daoyeyemi@aol.com>
2020-08-05 23:27:25 -0700

    made prototype row for schedule

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+
+    // var plannerSpace = $("");
+    // for (time = 9; time < 17; time++) {
+    //     value = time - 9;
 // Create timeblocks
-$("#plannerSpace").append()
-$("#timeBox").append("<span class='input-group-text' id='basic-addon1'>9 am</span>")
-$("#inputBox").append("<input type='text' class='form-control'>")
-$("#saveBox").append("<button class='btn btn-outline-secondary' type='button' id='button-addon2'>Save</button>")
+$("#plannerSpace").append("<div class='row'>");
+// console.log(row);
+$(".row").append("<div class='col-md-2' id='timeBox'>");
+$("#timeBox").append('<span class="input-group-text" id="basic-addon1">@</span>');
+$(".row").append("<div class='col-md-8' id='inputBox'>");
+$("#inputBox").append('<input type="text" class="form-control" placeholder="Enter tasks here..." aria-label="Username" aria-describedby="basic-addon1">');
+$(".row").append("<div class='col-md-2' id='saveBox'>");
+$("#saveBox").append('<button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>');
+
+
+// Time: 9 am
+
 // Create buttons for saving info in box  
 // Create date on top of page  
-// Color code according to hour in workday
+// Color code according to hour in work day
